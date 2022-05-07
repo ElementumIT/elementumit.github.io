6f8b7dfa43676dfff585e4bb466ccb7a7b609653
Brett Olges <brettolges@gmail.com>
2022-05-07 08:16:36 -0500

    cp

--- src/app/page-services/page-services.component.ts
+++ src/app/page-services/page-services.component.ts
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-services.component.less']
 })
 export class PageServicesComponent implements OnInit {
-
+tellMore = false;
   constructor() { }
-
   ngOnInit(): void {
   }
 
