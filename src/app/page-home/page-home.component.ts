c6e05c5a5039918eb1c2acb1f248a27ca7a96112
Brett Olges <brettolges@gmail.com>
2025-02-03 07:27:35 -0600

    cp

--- src/app/page-home/page-home.component.ts
+++ src/app/page-home/page-home.component.ts
@@ -2,6 +2,7 @@ import { SimpleGraphService } from './../services/SimpleGraphService.module';
 import { Component, OnInit } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-page-home',
@@ -11,7 +12,7 @@ import { throwError } from 'rxjs';
 export class PageHomeComponent implements OnInit {
   public queryResult: any;
 
-  constructor(private simpleGraphService: SimpleGraphService) {}
+  constructor(private simpleGraphService: SimpleGraphService, private http: HttpClient) {}
   ngOnInit(): void {}
   tryGraphQuery() {
     this.simpleGraphService
@@ -36,5 +37,17 @@ export class PageHomeComponent implements OnInit {
         console.log(x);
         this.queryResult = x;
       });
+  }
+
+  getNotionTasks() {
+    this.http.get('https://us-central1-elementumit-hello-world.cloudfunctions.net/get-notion-tasks')
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.error('Error fetching Notion tasks:', error);
+        }
+      );
   }
 }
